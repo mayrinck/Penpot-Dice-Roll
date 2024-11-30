ab6cdda08a96b84ee5da3b226990f56485c42f38
Renan Mayrinck <renan.mtk@gmail.com>
2024-11-29 23:36:26 -0300

    limpei aqui

--- src/plugin.ts
+++ src/plugin.ts
@@ -3,19 +3,6 @@ penpot.ui.open("Lucky Tools!", `?theme=${penpot.theme}`, {
   height: 370,
 });
 
-penpot.ui.onMessage<string>((message) => {
-  if (message === "create-text") {
-    const text = penpot.createText("Hello world!");
-
-    if (text) {
-      text.x = penpot.viewport.center.x;
-      text.y = penpot.viewport.center.y;
-
-      penpot.selection = [text];
-    }
-  }
-});
-
 // Update the theme in the iframe
 penpot.on("themechange", (theme) => {
   penpot.ui.sendMessage({
